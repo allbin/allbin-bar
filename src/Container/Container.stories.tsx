aa18768a471c22164a9d6d381dd25732bfd51740
alendri <alendri@allbin>
2019-12-13 15:25:12 +0100

    Added optional language picker. Closes #3

--- src/Container/Container.stories.tsx
+++ src/Container/Container.stories.tsx
@@ -20,18 +20,62 @@ const bgDiv = {
     backgroundColor: '#F1F4FF',
 };
 
-storiesOf('AllbinBar', module).add('Container', () => (
-    <div style={bgDiv}>
-        <Container
-            sso={sso}
-            open={true}
-            onClose={action('onClose')}
-            onDashboard={action('onDashboard')}
-            title="IWA"
-            current_version="v1.0.1"
-            show_about_btn={true}
-            show_help_btn={true}
-            tool_info={['dummy tool information 1', 'dummy tool information 2']}
-        />
-    </div>
-));
+storiesOf('AllbinBar', module)
+    .add('Container', () => (
+        <div style={bgDiv}>
+            <Container
+                sso={sso}
+                open={true}
+                onClose={action('onClose')}
+                onDashboard={action('onDashboard')}
+                onLanguage={action('onLanguage')}
+                language={'sv-SE'}
+                title="IWA"
+                current_version="v1.0.1"
+                show_about_btn={true}
+                show_help_btn={true}
+                tool_info={[
+                    'dummy tool information 1',
+                    'dummy tool information 2',
+                ]}
+            />
+        </div>
+    ))
+    .add('Container no logout', () => (
+        <div style={bgDiv}>
+            <Container
+                sso={sso}
+                open={true}
+                onClose={action('onClose')}
+                onDashboard={action('onDashboard')}
+                title="IWA"
+                current_version="v1.0.1"
+                show_about_btn={true}
+                show_help_btn={true}
+                show_logout_btn={false}
+                tool_info={[
+                    'dummy tool information 1',
+                    'dummy tool information 2',
+                ]}
+            />
+        </div>
+    ))
+    .add('Container minimal', () => (
+        <div style={bgDiv}>
+            <Container
+                open={true}
+                onClose={action('onClose')}
+                onDashboard={action('onDashboard')}
+                title="No SSO Test"
+                current_version="v1.0.1"
+                show_about_btn={false}
+                show_help_btn={false}
+                show_logout_btn={false}
+                show_dashboard_btn={false}
+                tool_info={[
+                    'dummy tool information 1',
+                    'dummy tool information 2',
+                ]}
+            />
+        </div>
+    ));
